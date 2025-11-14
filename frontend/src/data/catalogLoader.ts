1fb404d706d2fc5cd19780b9ae355f3f229174f5
LauraWaldron <97552606+LauraWaldron@users.noreply.github.com>
2025-11-14 18:30:10 -0500

    added the json files

---
+++ frontend/src/data/catalogLoader.ts
@@ -0,0 +1,17 @@
+export const loadCatalog = async (major: string) => {
+  switch (major) {
+    case "BSComputerScience":
+      return await import("./catalog - BSComputerScience.json");
+    case "BSDataScience&A":
+      return await import("./catalog - BSDataScience&A.json");
+    case "BAComputerScience":
+      return await import("./catalog - BAComputerScience.json");
+    case "BSMechanical":
+      return await import("./catalog - BSMechanical.json");
+    case "BSCivil":
+      return await import("./catalog - BSCivil.json");
+    // add the rest…
+    default:
+      throw new Error("Unknown major: " + major);
+  }
+};
